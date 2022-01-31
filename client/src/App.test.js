41f0d93b4624ac57c0ce517503b99ad6a8662785
Jay Clark <jay@jayeclark.dev>
2022-01-30 20:41:16 -0400

    Test continuous integration
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders username column', () => {
   render(<App />);
   const linkElement = screen.getByText(/username/i);
   expect(linkElement).toBeInTheDocument();
